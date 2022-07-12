184d88767a4aeb08b765302032abeaf1dc1b953f
Garret Gallant <gallanttechnological@gmail.com>
2022-07-11 20:08:17 -0600

    Added a click listener on form input. Need to debug

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 //javascript for YodaSpeak API 
 console.log("Hello world!")
 
-const placeholderUserInput = document.querySelector('#userInput') //this is the value of the form text box I think
 let url = 'https://api.funtranslations.com/translate/yoda.json'
 
-
-function getYodaData(userInput){
-    let userInput = "Something has been put here"
-    let newUrl = `${url}?text=${userInput}`
+function getYodish(stringToConvert){
+    let newUrl = `${url}?text=${stringToConvert}`
 fetch(newUrl)
 .then((data)=>data.json())
-.then((data) => {console.log(data)})
+.then((data) => console.log(data))
 }
 
-// console.log(getYodaData(userInput))
+const userTextBox = document.getElementsByClassName('new-yodish')
+// userTextBox.addEventListener('click', function(e){
+//     e.preventDefault()
+
+//     let userInput = userTextBox.value
+//     console.log(userInput)
+// })
+
+console.log(userTextBox)
+// console.log(userInput)
+
+
+
+
+
+// console.log(getYodish(userInput))
