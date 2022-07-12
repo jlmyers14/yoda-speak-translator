a0a7fbaa2bfea9034abf75adc7157951cc30e540
Garret Gallant <gallanttechnological@gmail.com>
2022-07-11 19:21:26 -0600

    EOD Commit

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //javascript for YodaSpeak API 
 console.log("Hello world!")
+
+const placeholderUserInput = document.querySelector('#userInput') //this is the value of the form text box I think
+let url = 'https://api.funtranslations.com/translate/yoda.json'
+
+
+function getYodaData(userInput){
+    let userInput = "Something has been put here"
+    let newUrl = `${url}?text=${userInput}`
+fetch(newUrl)
+.then((data)=>data.json())
+.then((data) => {console.log(data)})
+}
+
+// console.log(getYodaData(userInput))
