f2cad14740c353cc3deb0c4c9e63c16d8ae9d980
Garret Gallant <gallanttechnological@gmail.com>
2022-07-12 17:55:54 -0600

    added gitignore, added custom fonts, built full api functionality with accurate returned strings, added background image, styled page with better design, modified page colors, id'd more css elements

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
-//javascript for YodaSpeak API 
+var API_KEY = config.API_KEY
+
+//global declarations
 let url = 'https://api.funtranslations.com/translate/yoda.json'
-
-function getYodish(stringToConvert){
-    let newUrl = `${url}?text=${stringToConvert}`
-    fetch(newUrl)
-    .then((data)=>data.json())
-    .then((data) => console.log(data))
-}
-
+let yodishObject = {}
 const userTranslateButton = document.getElementById('user-input')
 const userTextBox = document.getElementById('new-yodish-string')
+const newYodishStringOutput = document.getElementById('output')
 
-
-
-// userTranslateButton.addEventListener('click', function takeUserInput(e){
-//     e.preventDefault();
+//function to apply a string to the yodish api
+function getYodish(stringToConvert){
+    let newUrl = `${url}?text=${stringToConvert}`
     
-//     let userInput = userTextBox.value
-//     const yodishString = (getYodish(userInput))
-//     console.log(yodishString.translated)
-// })
+    const configPolicy = {
+        method: "GET",
+        headers: {
+          "X-FunTranslations-Api-Secret": API_KEY
+        }
+      }
+
+    fetch(newUrl, configPolicy)
+    .then((data) => data.json())
+    .then((data) => yodishObject = data)
+}
+
+//eventlistener that runs the getYodish function with a user provided string to return conversion
+userTranslateButton.addEventListener('click', function takeUserInput(e){
+    e.preventDefault();
+    
+    let userInput = userTextBox.value
+    getYodish(userInput)
+    const yodishString = yodishObject.contents.translated
+    console.log(yodishString)
+
+    //appends
+    const stringToOutput = document.createElement('p')
+    stringToOutput.textContent = yodishString
+    newYodishStringOutput.append(stringToOutput)
+})
+
+// function renderNewYodish(){
+//     const stringToOutput = document.createElement('p')
+//     stringToOutput.textContent = "this string needs to be yodish" //replace with relevant data once we can. Optimally this data is the translated yodish returned by our API
+//     stringToOutput.setAttribute('id', 'string-output')
+//     newYodishStringOutput.append(stringToOutput)
+// }
+
+
 
