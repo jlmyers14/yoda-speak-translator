e2e954e11342a95991562aea5f2959f01253a97f
Garret Gallant <gallanttechnological@gmail.com>
2022-07-13 18:39:18 -0600

    fixed API_KEY recognition, identified possible cause of double click bug, added theme button that dynamically changes page color and background, added to anon translate function (generate array), created addToHistory function, appends array elements from upvoteArray to list on left side of page, fixed, debugged, and modified css, added to html

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var API_KEY = config.API_KEY
-//GOALS FOR THE REMAINDER OF THE PROJECT
-// ! debug double click issue
-// ! Figure out why it zooms every time we click
-// ! Take a look at the weird 2 body thing
-// ! Add 2 more event listeners (light/dark theme sith yoda) (upvote/save favorite yoda-isms)
-// ! implement array iteration
+const {API_KEY} = config.API_KEY
+// !GOALS FOR THE REMAINDER OF THE PROJECT!
+//  Debug double click issue
+//Look into scaling the website to different resolutions
+//  Add css styling to the favorites and yodism history
+//  Change box text to red when switching to the dark side
+//  Change Master at top to Sith when switching to the dark side
 
 //global declarations
 let url = 'https://api.funtranslations.com/translate/yoda.json'
@@ -12,38 +12,74 @@ let yodishObject = {}
 const userTranslateButton = document.getElementById('user-input')
 const userTextBox = document.getElementById('new-yodish-string')
 const newYodishStringOutput = document.getElementById('output')
+const lightDarkButton = document.getElementById('light-dark')
+const history = document.getElementById('yodism-history-nest')
+const upvoteArray = []
+let image1 = true;
+document.body.style.color = '#2FF924';
 
 //function to apply a string to the yodish api
+
 function getYodish(stringToConvert){
-    let newUrl = `${url}?text=${stringToConvert}`
-    
-    const configPolicy = {
-        method: "GET",
-        headers: {
-          "X-FunTranslations-Api-Secret": API_KEY
-        }
-      }
-
-    fetch(newUrl, configPolicy)
-    .then((data) => data.json())
-    .then((data) => yodishObject = data)
+  const configPolicy = {
+    method: "GET",
+    headers: {
+      "X-FunTranslations-Api-Secret": API_KEY
+    }
+  }
+  let newUrl = `${url}?text=${stringToConvert}`
+  
+  fetch(newUrl, configPolicy)
+  .then((data) => data.json())
+  .then((data) => {
+    yodishObject = data.contents
+  })  
 }
+//How to fix this double click issue?
 
-//eventlistener that runs the getYodish function with a user provided string to return conversion
-userTranslateButton.addEventListener('click', function takeUserInput(e){
-    e.preventDefault();
-    
-    let userInput = userTextBox.value
-    getYodish(userInput)
-    const yodishString = yodishObject.contents.translated
-    console.log(yodishString)
+//eventlistener that allows user to switch between the light and dark side
+lightDarkButton.addEventListener('click', function (e) {
+  e.preventDefault();
 
-    //Take yodish said prior, and put it in an array. use that array to add yoda-isms log on a different page
-    const stringToOutput = document.createElement('p')
-    stringToOutput.textContent = yodishString
-    newYodishStringOutput.append(stringToOutput)
+  if(image1 === true) {
+    document.body.style.backgroundImage = "url('assets/SithYoda.png')";
+    //go to image2
+    image1 = false;
+    document.body.style.color = '#EB212E';
+  }
+  else if(image1 === false) {
+    document.body.style.backgroundImage = "url('assets/yoda1.jpg')";
+    //revert to image1
+    image1 = true;
+    document.body.style.color = '#2FF924';
+  }
 })
 
+//eventlistener that runs the getYodish function with a user provided string to return conversion
+userTranslateButton.addEventListener('click', function (e){
+    e.preventDefault();
 
+    let userInput = userTextBox.value
+    getYodish(userInput)
+    let yodishString = yodishObject.translated
 
+    let stringToOutput = document.createElement('p')
+    stringToOutput.textContent = yodishString
+    newYodishStringOutput.append(stringToOutput)
 
+    upvoteArray.push(stringToOutput.textContent)
+    console.log(upvoteArray)
+    
+    addToHistory(upvoteArray)
+})
+
+//function that takes an array as a parameter, and populates the menu Tyler creates with values from it.
+const addToHistory = upvoteArray => {
+  userTranslateButton.addEventListener('click', function(e){
+    e.preventDefault();
+    const historyText = upvoteArray.slice(-1)
+    let tr = document.createElement('tr')
+    tr.textContent = historyText
+    history.append(tr)
+  })
+}
